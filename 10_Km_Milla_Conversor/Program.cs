bdca67480536863a99859601ea70be0d051d334b
Vladimir Ascencio <165211702+Ascencio7@users.noreply.github.com>
2025-11-17 17:37:01 -0600

    Conversor de KM y Millas
    Programa conversor de km a millas y viceversa

---
+++ 10_Km_Milla_Conversor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Km_Milla_Conversor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // iniciar la variable
+            double kilometro, millas;
+
+            // solicitar los datos
+            Console.Write("\nIngresa los Kilometros: ");
+            kilometro = Convert.ToDouble(Console.ReadLine());
+
+            // se llama la funcion
+            millas = ConvertirKMaMillas(kilometro);
+
+            // mostrar el total
+            Console.WriteLine($"\nLos {kilometro} kms a millas son: {millas} millas.");
+            Console.WriteLine("\n");
+        }
+
+        #region Conversor
+        public static double ConvertirKMaMillas(double kilometro)
+        {
+            return Math.Round(kilometro * 0.621371, 5);
+        }
+        #endregion
+
+    }
+}
---
+++ 10_Km_Milla_Conversor/README.md
@@ -0,0 +1,1 @@
+# Km_Milla_Conversor
