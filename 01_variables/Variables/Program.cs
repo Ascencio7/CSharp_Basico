8c0967bf29432a8a08870f8e826b2d6f70be274f
Vladimir Ascencio <165211702+Ascencio7@users.noreply.github.com>
2025-11-17 17:06:12 -0600

    1. Las variables en C#

---
+++ 01_variables/Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿// Las variables en C#
+using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables de tipo entero
+            int numEntero = 10;
+
+            // Variable de tipo flotante
+            double numFloa = 10.48D;
+
+            float numFlotante = 5.75F;
+
+            // Variable de tipo string
+            string mensaje = "Soy Vladimir Ascencio";
+
+            // Variables de tipo booleano
+            bool isTrue = false;
+            bool isFalso = true;
+
+            // Mostrar los resultados
+
+            System.Console.WriteLine($"\nValor entero: {numEntero}");
+            System.Console.WriteLine($"Valor double: {numFloa}");
+            System.Console.WriteLine($"Valor flotante: {numFlotante}");
+            System.Console.WriteLine($"Valor String: {mensaje}");
+            System.Console.WriteLine($"Valor booleano 1: {isTrue}");
+            System.Console.WriteLine($"Valor booleano 2: {isFalso}\n");
+
+        }
+    }
+}
