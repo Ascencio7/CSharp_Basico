8df985f3e453a48fa93d2052bfc66057eac1b09a
Vladimir Ascencio <165211702+Ascencio7@users.noreply.github.com>
2025-11-17 17:36:13 -0600

    Conversor de °C a °F
    Programa para convertir grados Celsius a Fahrenheit

---
+++ 09_Celcius_Fahrenheit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celcius_Fahrenheit
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // iniciar las variables
+            double celcius, fahrenheit;
+
+            // solicitar los datos
+            Console.Write("\nIngresa la temperatura en Celsius: ");
+            celcius = Convert.ToDouble(Console.ReadLine());
+
+            // llamar la funcion
+            fahrenheit = CelsiusFahrenheit(celcius);
+
+            // mostrar el resultado
+            Console.WriteLine($"\nLa temperatura Celcius a Fahrenheit es {fahrenheit} °F");
+            Console.WriteLine("\n");
+        }
+
+        #region Conversor
+        public static double CelsiusFahrenheit(double celcius)
+        {
+            return Math.Round((celcius * 9 / 5) + 32, 2);
+        }
+        #endregion
+
+    }
+}
