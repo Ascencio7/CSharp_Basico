933c56e1cfc1e3326ade32dbb19dd4535d002db4
Vladimir Ascencio <165211702+Ascencio7@users.noreply.github.com>
2025-11-17 18:23:56 -0600

    Condicional para votar
    Programa que evalúa condiciones por edad para poder votar

---
+++ 12_votar_condicion/votar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace votar
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Verificar si una persona puede votar o no
+
+            // Declarar las variables
+            int edad;
+            string entrada;
+
+            // Pedir la edad
+            Console.Write("\nIngresa tu edad: ");
+            entrada = Console.ReadLine();
+            edad = Convert.ToInt32(entrada);
+
+            // Evaluar las condiciones
+            if (edad < 0)
+            {
+                Console.WriteLine("\nLa edad ingresada es invalida.");
+            }
+            else if (edad < 18)
+            {
+                Console.WriteLine("\nEres menor de edad, no puedes votar.");
+            }
+            else if (edad >= 18 && edad <= 69)
+            {
+                Console.WriteLine("\nEres mayor de edad, puedes votar.");
+            }
+            else
+            {
+                Console.WriteLine("\nEres adulto mayor, el voto es opcional.");
+            }
+
+            Console.WriteLine("Fin del programa.\n");
+
+        }
+    }
+}
