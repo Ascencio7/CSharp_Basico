7cb80f4a8bc6969b9b55223b7d65c352ab067220
Vladimir Ascencio <165211702+Ascencio7@users.noreply.github.com>
2025-11-17 18:12:08 -0600

    Condiciones IF, ELSE y ELSE IF
    Programa sencillo si un numero es positivo, negativo o 0

---
+++ 11_condicionales/condicionales/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace condicionales
+{
+    public class Programa
+    {
+        static void Main(string[] args)
+        {
+            // Verificar si el numero es positivo, negativo o 0
+            string entrada;
+            int num;
+
+            // Solicitar el numero
+            Console.Write("\nIngresa el numero: ");
+            entrada = Console.ReadLine();
+            num = Convert.ToInt32(entrada);
+
+            // Verificar si el numero es positivo, negativo o 0
+            if (num > 0)
+            {
+                Console.WriteLine($"\nEl numero es positivo y mayor a 0: {num}");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine($"\nEl numero es negativo: {num}");
+            }
+            else if (num == 0)
+            {
+                Console.WriteLine("\nValor es 0.");
+            }
+            else
+            {
+                Console.WriteLine("\nValor nulo.");
+            }
+            Console.WriteLine("Fin del programa.\n");
+
+        }
+    }
+}
