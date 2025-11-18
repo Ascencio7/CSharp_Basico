1cfe682011fc6de3173415397f0400cb7a2cb95b
Vladimir Ascencio <165211702+Ascencio7@users.noreply.github.com>
2025-11-17 18:46:44 -0600

    Uso de Switch y Lista
    Programa que solicita un numero para verificar el dia de la semana

---
+++ 13_Dias_Semana/dias_semana/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dias_semana
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definir la lista de los dias de la semana de tipo string
+            List<string> dias = new List<string>
+            {
+                "Lunes","Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"
+            };
+
+            Console.Write("Ingresa un número del 1 al 7: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            switch (numero)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    // Resta 1 para ajustar el indice de la lista 0 a 6
+                    Console.WriteLine("El día es: " + dias[numero - 1]);
+                    break;
+
+                default:
+                    Console.WriteLine("\nEntrada inválida. Ingresa del 1 al 7.");
+                    break;
+            }
+
+            Console.WriteLine("\nFin del programa.\n");
+        }
+    }
+}
