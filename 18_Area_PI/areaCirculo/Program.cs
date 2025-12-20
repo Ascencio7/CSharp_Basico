6bc00b2756f8c224922e3b6b23deda0852d56dea
Vladimir Ascencio <165211702+Ascencio7@users.noreply.github.com>
2025-12-20 17:23:38 -0600

    Cálculo de área del círculo

---
+++ 18_Area_PI/areaCirculo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Declarar la variable de manera implicitas
+        var radio = 0d; // el 'd' indico que sea un double
+        var resultado = 0d;
+
+        // Solicitar el dato
+        Console.Write("\nIngresa el valor del radio: ");
+        radio = Convert.ToDouble(Console.ReadLine());
+
+        resultado = Math.PI * Math.Pow(radio, 2);
+        
+        // Redondear el resultado
+        resultado = Math.Round(resultado, 2);
+
+        // Se muestra el resultado
+        Console.WriteLine($"\nEl radio del circulo es: {resultado}");
+        Console.WriteLine("Fin del programa.\n");
+    }
+}
