1cfd6245e1e6596853ee9348d764c0245d6ec421
Vladimir Ascencio <165211702+Ascencio7@users.noreply.github.com>
2026-03-29 19:41:17 -0600

    Fecha de Nacimiento
    Código C# sobre conocer la edad dependiendo de su fecha de nacimiento

---
+++ FechaNacimiento/FechaNacimiento/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FechaNacimiento
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // fecha de nacimiento
+            DateOnly fechaNacimiento = new DateOnly(2003, 9, 24);
+
+            // fecha actual
+            DateOnly fechaActual = DateOnly.FromDateTime(DateTime.Now);
+
+            // calcular edad
+            int edad = fechaActual.Year - fechaNacimiento.Year;
+
+            // ajustar si aún no ha cumplido años este año
+            if (fechaActual < fechaNacimiento.AddYears(edad))
+            {
+                edad--;
+            }
+
+            // mostrar la edad
+            Console.WriteLine("La edad es: " + edad);
+        }
+    }
+}
